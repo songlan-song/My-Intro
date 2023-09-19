757a9ec0ac3c8219231773aec0d8d6b2e0d28cd2
lan song <lansongbjut@163.com>
2023-09-19 17:44:22 +0100

    finish starssky

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,87 +1,138 @@
-<template></template>
+<template>
+    <div class="home">
+        <div class="canvas-container" ref="screenDom"></div>
+    </div>
+</template>
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
-//创建场景
-const scene = new THREE.Scene()
+import { gsap } from "gsap"
+let screenDom = ref(null)
+onMounted(() => {
+    //创建场景
+    const scene = new THREE.Scene()
 
-//创建相机
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+    //创建相机
+    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.set(0, 0, 10)
 
-//创建渲染器
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-//实例化加载器
-const gltfLoader = new GLTFLoader()
-//加载模型
-gltfLoader.load("/scene.gltf", (gltf) => {
-    let vertices = [];
-    let colors = [];
-
-    // 遍历模型中的每一个mesh
-    gltf.scene.traverse((child) => {
-        if (child.isMesh) {
-            let positionArray = child.geometry.attributes.position.array;
-            for (let i = 0; i < positionArray.length; i += 3) {
-                // 提取顶点
-                vertices.push(positionArray[i], positionArray[i + 1], positionArray[i + 2]);
-
-                // 为每个顶点分配一个随机颜色
-                const r = Math.random();
-                const g = Math.random();
-                const b = Math.random();
-                colors.push(r, g, b);
-            }
-        }
-    });
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
-    geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-
-    const material = new THREE.PointsMaterial({ size: 0.01, vertexColors: true });
-    const nebula = new THREE.Points(geometry, material);
-    scene.add(nebula);
-    const nebulaRotationSpeed = 0.01;
-    //设置相机位置
-    camera.position.z = 5
-    camera.position.y = 3
-    camera.position.x = 2
-    camera.lookAt(0, 0, 0)
-
-    function animate() {
-        // 更新星云的旋转
-        nebula.rotation.x += nebulaRotationSpeed;
-        nebula.rotation.y += nebulaRotationSpeed;
-
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate);
-    }
-    animate();
-});
-
-//添加世界坐标辅助器(括号中为线段长度)
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
-
-//添加轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement)
-//设置带阻尼的惯性
-controls.enableDamping = true
-
-//监听窗口变化，重重窗口大小
-window.addEventListener("resize", () => {
-    //充值渲染器宽高比
+    //创建渲染器
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(window.innerWidth, window.innerHeight)
-    //重置相机宽高比
-    camera.aspect = window.innerWidth / window.innerHeight
-    //更新相机投影矩阵
-    camera.updateProjectionMatrix()
+    //将画布添加到页面中
+    screenDom.value.appendChild(renderer.domElement)
+
+    //创建时钟
+    const clock = new THREE.Clock();
+    let mixer; // 在最外部定义mixer
+
+    //创建星空背景
+    let url = "/images.png"
+    let envTexture = new THREE.TextureLoader().load(url)
+    envTexture.mapping = THREE.EquirectangularReflectionMapping
+    scene.background = envTexture
+    scene.environment = envTexture
+
+    //创建模型
+    let gltfLoader = new GLTFLoader()
+    gltfLoader.load("/robot/scene.gltf", (gltf) => {
+        // gltf.scene.scale.set(0.1,0.1,0.1)
+        gltf.scene.position.set(3, 0, 0)
+        scene.add(gltf.scene)
+
+         mixer = new THREE.AnimationMixer(gltf.scene); // 假设您想对整个场景进行动画。
+        // 对于gltf模型中的每一个动画...
+        gltf.animations.forEach((clip) => {
+            // 创建一个动画操作并播放它
+            const action = mixer.clipAction(clip);
+            action.play();
+        });
+
+        //监听鼠标滚动移动相机
+        window.addEventListener("mousewheel",e=>{
+            //往下滚
+            if(e.wheelDelta <0){
+
+            }
+            //往上滚
+            else if(e.wheelDelta > 0){
+
+            }
+        })
+    })
+
+   gltfLoader.load("/moon/scene.gltf",gltf=>{
+    // gltf.scene.scale.set(0.1,0.1,0.1)
+    // scene.add(gltf.scene)
+
+    let moon = gltf.scene.children[0].children[0].children[0].children[0].children[0]
+    for (let j = 0; j < 10; j++) {
+      let moonInstance = new THREE.InstancedMesh(
+        moon.geometry,
+        moon.material,
+        100
+      );
+
+      // scene.add(moon);
+      for (let i = 0; i < 100; i++) {
+        let x = Math.random() * 1000 - 500;
+        let y = Math.random() * 1000 - 500;
+        let z = Math.random() * 1000 - 500;
+
+        let matrix = new THREE.Matrix4();
+        let size = Math.random() * 20 - 8;
+        matrix.makeScale(size, size, size);
+        matrix.makeTranslation(x, y, z);
+        moonInstance.setMatrixAt(i, matrix);
+      }
+
+      gsap.to(moonInstance.position, {
+        duration: Math.random() * 10 + 2,
+        z: -1000,
+        ease: "linear",
+        repeat: -1,
+      });
+    scene.add(moonInstance)
+}
+   }) 
+    function render() {
+        requestAnimationFrame(render)
+            if(mixer){
+                const delta = clock.getDelta(); // clock是THREE.Clock的实例，用于获取自上次调用以来经过的时间。
+                mixer.update(delta);  // 更新mixer，这将播放动画
+            }
+
+            renderer.render(scene, camera)
+        }
+        render()
+
+    //创建光源
+    let light = new THREE.DirectionalLight(0xffffff, 1)
+    light.position.set(0, 0, 1)
+    scene.add(light)
+    let light2 = new THREE.DirectionalLight(0xffffff, 0.5)
+    light.position.set(0, 0, -1)
+    scene.add(light2)
+    let light3 = new THREE.DirectionalLight(0xffffff, 0.5)
+    light.position.set(-1, 1, 1)
+    scene.add(light3)
+
+    //监听窗口变化，重重窗口大小
+    window.addEventListener("resize", () => {
+        //充值渲染器宽高比
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        //重置相机宽高比
+        camera.aspect = window.innerWidth / window.innerHeight
+        //更新相机投影矩阵
+        camera.updateProjectionMatrix()
+    })
+
 })
+
+
+
 
 
 </script>
