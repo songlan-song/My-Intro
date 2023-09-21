76fd54f25e5eee79091b576510d5489aae04f24f
lan song <lansongbjut@163.com>
2023-09-21 18:02:12 +0100

    finish three countries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <div class="wrap">
             <div class="intro-container"  id="intro-container">
-            <div class="canvas-container" ref="screenDom" id="canvas-container"></div>
+            <div class="canvas-container" ref="screenDom" id="canvas-container" ></div>
             <div class="scrollCharacter-container">
                 <ul class="all-charters">
                     <li class="text1" ref="text1" style="opacity: 1;">1111111111111111111111111111111</li>
@@ -14,9 +14,57 @@
         </div>
         </div>
         <div class="tour-container">
-            <div class="UK-tour"></div>
-            <div class="Spain-tour"></div>
-            <div class="France-tour"></div>
+            <div class="UK-tour">
+                <div class="UK-chracter">
+                    <ul>
+                    <li>1111111111111111111111111111111</li>
+                    <li>2222222222222222222222222222222</li>
+                    <li>3333333333333333333333333333333</li>
+                    <li>4444444444444444444444444444444</li>
+                    </ul>
+                </div>
+                <div class="UK-more-information" ref="franceMoreInformation" ></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
+                <div class="moreinfo-character">
+                    <span>探索更多</span>
+                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                </div>
+                </div>
+            </div>
+            <div class="Spain-tour">
+                <div class="Spain-chracter">
+                    <ul>
+                    <li>1111111111111111111111111111111</li>
+                    <li>2222222222222222222222222222222</li>
+                    <li>3333333333333333333333333333333</li>
+                    <li>4444444444444444444444444444444</li>
+                    </ul>
+                </div>
+                <div class="Spain-more-information" ref="franceMoreInformation" ></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
+                <div class="moreinfo-character">
+                    <span>探索更多</span>
+                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                </div>
+                </div>
+            </div>
+            <div class="France-tour">
+                <div class="France-chracter">
+                    <ul>
+                    <li>1111111111111111111111111111111</li>
+                    <li>2222222222222222222222222222222</li>
+                    <li>3333333333333333333333333333333</li>
+                    <li>4444444444444444444444444444444</li>
+                    </ul>
+                </div>
+                <div class="France-more-information" ref="franceMoreInformation" ></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
+                <div class="moreinfo-character">
+                    <span>探索更多</span>
+                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -33,6 +81,14 @@ const text1 = ref(null)
 const text2 = ref(null)
 const text3 = ref(null)
 const text4 = ref(null)
+const imageOpacity = ref(0)
+function handleMouseEnter(){
+        imageOpacity.value=0.4
+        
+    }
+function handleMouseLeave(){
+    imageOpacity.value=0
+}
 onMounted(() => {
     //创建场景
     const scene = new THREE.Scene()
@@ -80,7 +136,6 @@ onMounted(() => {
             let y = (e.clientY / window.innerHeight) * 2 - 1;
             let timeline = gsap.timeline();
             timeline.to(gltf.scene.children[0].children[0].children[0].children[0].children[1].rotation, {
-                duration: 0.5,
                 x: y,
                 y: x,
                 duration: 1,
@@ -178,26 +233,25 @@ onMounted(() => {
     //文字滚动高亮效果
     gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.create({
-        trigger: "#app",
-        start: "top top",
-        end: "+=800",
-        scrub: true,
-        pin: true,
-        animation: gsap.timeline()
-            .to(".text1", { opacity: 1, duration: 1 }) // 淡入
-            .to(".text1", { opacity: 0.3, duration: 1 }) // 淡出
-            .to(".text2", { opacity: 1, duration: 1.5 })
-            .to(".text2", { opacity: 0.3, duration: 1.5 })
-            .to(".text3", { opacity: 1, duration: 1.5 })
-            .to(".text3", { opacity: 0.3, duration: 1.5 })
-            .to(".text4", { opacity: 1, duration: 1 })
-
-    });
+    trigger: ".wrap",
+    start: "top top",
+    end: "+=800",
+    scrub: true,
+    pin: true,
+    animation: gsap.timeline()
+        .to(".text1", { opacity: 1, duration: 1 })
+        .to(".text1", { opacity: 0.3, duration: 1 })
+        .to(".text2", { opacity: 1, duration: 1.5 })
+        .to(".text2", { opacity: 0.3, duration: 1.5 })
+        .to(".text3", { opacity: 1, duration: 1.5 })
+        .to(".text3", { opacity: 0.3, duration: 1.5 })
+        .to(".text4", { opacity: 1, duration: 1 })
+});
 
     //intro-container淡出效果
     ScrollTrigger.create({
     trigger: ".tour-container",
-    start: "top top",
+    start: "top 800px",
     end: "+=800",
     scrub: true,
     onEnter: () => {
@@ -224,21 +278,31 @@ onMounted(() => {
 
 })
 
+
 </script>
 <style  scoped>
+
+
 .wrap{
-    width: 100%;
+    width: 1494px;
     height: 100vh;
     position: relative;
-    perspective: 1000px
+    perspective: 1000px;
+    overflow-x: hidden;
+
+}
+
+.canvas-container{
+    overflow-x: hidden;
 }
 .intro-container{
-    width: 100%;
+    width: 1494px;
     height: 100vh;
-    overflow: hidden;
+    overflow: hidden !important;
     margin:0 auto;
     transform-origin: center center;
     position: absolute;
+    overflow-x: hidden;
 }
 
 .home {
@@ -263,25 +327,138 @@ onMounted(() => {
     overflow-wrap: break-word;
 }
 .tour-container{
-    widows: 100%;
+    width: 100%;
     height: 100vh;
     margin-top: 0;
 }
 .UK-tour {
     width: 100%;
-    height: 33vh;
-    background-color: yellow;
+    height: 100vh;
+    background:url("/UK/2.png");
+    background-size: cover;
+    position: relative;
+}
+.UK-chracter{
+    width: 30vw;
+    height: 55vh;
+    position: absolute;
+    top: 20vh;
+}
+.UK-chracter li{
+    display: inline;
+    line-height: 1.2;
+    font-weight: 600;
+    font-size: 48px;
+    font-weight: 700;
+    color: #fff;
+    opacity: 0.8;
+    overflow-wrap: break-word;
 }
 
 .Spain-tour {
     width: 100%;
-    height: 33vh;
-    background-color: green
+    height: 100vh;
+    background:url("/Spain/4.jpg");
+    background-size: cover;
+    position: relative;
+}
+.Spain-chracter{
+    width: 30vw;
+    height: 55vh;
+    position: absolute;
+    top: 20vh;
+}
+.Spain-chracter li{
+    display: inline;
+    line-height: 1.2;
+    font-weight: 600;
+    font-size: 48px;
+    font-weight: 700;
+    color: #fff;
+    opacity: 0.8;
+    overflow-wrap: break-word;
 }
 
 .France-tour {
     width:100%;
-    height: 33vh;
-    background-color: aqua
+    height: 100vh;
+    background:url("/France/2.jpg");
+    background-size: cover;
+    position: relative;
 }
+.France-chracter{
+    width: 30vw;
+    height: 55vh;
+    position: absolute;
+    top: 20vh;
+}
+.France-chracter li{
+    display: inline;
+    line-height: 1.2;
+    font-weight: 600;
+    font-size: 48px;
+    font-weight: 700;
+    color: #fff;
+    opacity: 0.8;
+    overflow-wrap: break-word;
+}
+.France-more-information{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    width: 200px;
+    height: 200px;
+    background: url("/France/1.png");
+    background-size: cover;
+    cursor: pointer;
+}
+.UK-more-information{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    width: 200px;
+    height: 200px;
+    background: url("/UK/3.png");
+    background-size: cover;
+    cursor: pointer;
+}
+.Spain-more-information{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    width: 200px;
+    height: 200px;
+    background: url("/Spain/1.jpg");
+    background-size: cover;
+    cursor: pointer;
+}
+
+.cover-black{
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    width: 200px;
+    height: 200px;
+    background: url("/black.jpg");
+    background-size: cover;
+    opacity: 0;
+    cursor: pointer;
+}
+.moreinfo-character{
+    position:absolute;
+    bottom: 0;
+    width: 100%;
+    height: 30px;
+    margin-bottom: 10px;
+
+}
+.moreinfo-character span{
+    color: #fff;
+    font-weight: 700;
+    position: absolute;
+    left: 65px;
+    opacity: 1;
+}
+
+
 </style>
