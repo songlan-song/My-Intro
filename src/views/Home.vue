c5d92b488c526f4599f0f6f06fbae3c5baa1f4db
lan song <lansongbjut@163.com>
2023-09-23 00:18:01 +0100

    part of comment finished 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
     <div class="home">
         <div class="wrap">
             <div class="intro-container" id="intro-container">
+                <div class="logo-container">
+                    <div class="logo"></div>
+                    <span>songsong.com</span>
+                </div>
                 <div class="canvas-container" ref="screenDom" id="canvas-container"></div>
                 <div class="scrollCharacter-container">
                     <span style="font-size: 54px ;color: #fff;display: block;">Hi，你好! 我是</span>
@@ -11,8 +15,19 @@
                         <li class="text1" ref="text1" style="opacity: 1;">一个简单的代码爱好者,热衷于学习前端的各种技术，目前是一个前端开发工程师。</li>
                         <li class="text2" ref="text2">乐观和热情是我的礼物，不敲代码的时候，有时是玩家，有时是运动员。热爱LOL，热爱篮球，偶尔看剧，喜欢美食。</li>
                         <li class="text3" ref="text3">如果你也喜欢开源，喜欢创造，喜欢分享，欢迎在我的网站留言。</li>
-                        <li class="text4" ref="text4">这句话支持着我一直前进，希望你也能从中获取一些力量：勇敢是当你还未开始就已经知道自己会输，可你依然要去做，而且无论如何都要把它坚持到底，你很少能赢，但有时也会。</li>
+                        <li class="text4" ref="text4">
+                            这句话支持着我一直前进，希望你也能从中获取一些力量：勇敢是当你还未开始就已经知道自己会输，可你依然要去做，而且无论如何都要把它坚持到底，你很少能赢，但有时也会。</li>
                     </ul>
+                </div>
+                <div class="message-board-wrap">
+                    <div class="message-board">
+                        <el-form :model="form">
+                            <el-form-item class="comment-input-conainer">
+                                <el-input class="comment-input" v-model="form.name" placeholder="欢迎留言交流" style="width: 750px;" />
+                                <el-button  @click="onSubmit" style="margin-left: 20px;background-color: transparent;">发表</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,12 +36,15 @@
                 <div class="UK-chracter">
                     <span class="UK-name">伦敦</span>
                     <ul>
-                        <li>伦敦，这座历史悠久而又充满活力的城市，以其独特的魅力吸引着世界各地的游客。伦敦不仅是英国的首都，更是一个文化、艺术、历史和时尚的大熔炉，这里汇聚了无数的人文景观和自然美景，为每一位来访者提供了丰富多彩的体验。</li>
-                        <li>漫步在泰晤士河畔，您可以欣赏到大本钟和伦敦眼的壮丽景象，感受这座城市独特的历史韵味和现代气息的完美融合。白金汉宫的卫兵换岗仪式，是体验英国皇家传统的不二之选。塔桥更是以其独特的设计和卓越的工程技术成为了伦敦的象征之一。来到历史悠久的贝克街，仿佛能感受到福尔摩斯侦探故事中的神秘氛围。这里，每一扇门后都似乎藏着未解之谜，每一道巷口都充满了侦探故事的味道。</li>
+                        <li>伦敦，这座历史悠久而又充满活力的城市，以其独特的魅力吸引着世界各地的游客。伦敦不仅是英国的首都，更是一个文化、艺术、历史和时尚的大熔炉，这里汇聚了无数的人文景观和自然美景，为每一位来访者提供了丰富多彩的体验。
+                        </li>
+                        <li>漫步在泰晤士河畔，您可以欣赏到大本钟和伦敦眼的壮丽景象，感受这座城市独特的历史韵味和现代气息的完美融合。白金汉宫的卫兵换岗仪式，是体验英国皇家传统的不二之选。塔桥更是以其独特的设计和卓越的工程技术成为了伦敦的象征之一。来到历史悠久的贝克街，仿佛能感受到福尔摩斯侦探故事中的神秘氛围。这里，每一扇门后都似乎藏着未解之谜，每一道巷口都充满了侦探故事的味道。
+                        </li>
                     </ul>
                 </div>
                 <div class="UK-more-information" ref="franceMoreInformation"></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave"
+                    :style="{ opacity: imageOpacity }">
                     <div class="moreinfo-character">
                         <span>探索更多</span>
                         <TopRight
@@ -39,11 +57,13 @@
                     <span class="Spain-name">巴塞罗那</span>
                     <ul>
                         <li>毫无疑问，巴塞罗那是一座充满艺术氛围、热情开放的城市，她位于西班牙东北部，坐拥美丽的地中海海岸线。这座城市以其丰富的历史、独特的建筑、美味的美食和活力四射的文化生活而闻名于世。</li>
-                        <li>巴塞罗那是世界著名建筑大师安东尼·高迪的艺术殿堂。当您漫步在这座城市的大街小巷时，会不时地遇见高迪独具一格的建筑作品。其中，圣家堂无疑是最为人们熟知的地标之一，其独特的设计和宏伟的塔尖令人叹为观止。除此之外，帕克·吉厄尔、米拉之家和巴特约之家等都是高迪留给这座城市的珍贵遗产。</li>
+                        <li>巴塞罗那是世界著名建筑大师安东尼·高迪的艺术殿堂。当您漫步在这座城市的大街小巷时，会不时地遇见高迪独具一格的建筑作品。其中，圣家堂无疑是最为人们熟知的地标之一，其独特的设计和宏伟的塔尖令人叹为观止。除此之外，帕克·吉厄尔、米拉之家和巴特约之家等都是高迪留给这座城市的珍贵遗产。
+                        </li>
                     </ul>
                 </div>
                 <div class="Spain-more-information" ref="franceMoreInformation"></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave"
+                    :style="{ opacity: imageOpacity }">
                     <div class="moreinfo-character">
                         <span>探索更多</span>
                         <TopRight
@@ -56,13 +76,15 @@
                     <span class="France-name">巴黎</span>
                     <ul>
                         <li>巴黎，被誉为“光之城”，是一座充满浪漫与艺术氛围的城市。这里是时尚的发源地，艺术的殿堂，美食的天堂。巴黎的每一条街道、每一座建筑都仿佛在讲述着一段古老而浪漫的故事。</li>
-                        <li>艾菲尔铁塔是巴黎乃至全球最为人们所熟知的地标之一，她伫立在塞纳河畔，以其优雅的姿态和独特的设计吸引着世界各地的游客。当夜幕降临，艾菲尔铁塔上的灯光闪烁，整个城市都仿佛被浪漫的氛围所包围。</li>
+                        <li>艾菲尔铁塔是巴黎乃至全球最为人们所熟知的地标之一，她伫立在塞纳河畔，以其优雅的姿态和独特的设计吸引着世界各地的游客。当夜幕降临，艾菲尔铁塔上的灯光闪烁，整个城市都仿佛被浪漫的氛围所包围。
+                        </li>
                         <li>巴黎还是世界著名的艺术之都。卢浮宫博物馆收藏着丰富的艺术珍品，如《蒙娜丽莎》和《胜利女神像》等，是艺术爱好者的必游之地。</li>
                         <li>这座城市无论在哪个角落，都充满了浪漫和诗意，无论是漫步在塞纳河畔，还是欣赏夜晚的巴黎，都会让您感受到这座城市独特的魅力，留下难以忘怀的回忆。</li>
                     </ul>
                 </div>
                 <div class="France-more-information" ref="franceMoreInformation"></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave"
+                    :style="{ opacity: imageOpacity }">
                     <div class="moreinfo-character">
                         <span>探索更多</span>
                         <TopRight
@@ -74,7 +96,7 @@
     </div>
 </template>
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted, reactive } from 'vue';
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
@@ -88,6 +110,20 @@ const text3 = ref(null)
 const text4 = ref(null)
 const imageOpacity = ref(0)
 
+const form = reactive({
+    name: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
+})
+
+const onSubmit = () => {
+    console.log('submit!')
+}
 function handleMouseEnter() {
     imageOpacity.value = 0.4
 }
@@ -123,8 +159,7 @@ onMounted(() => {
     //创建模型
     let gltfLoader = new GLTFLoader()
     gltfLoader.load("/robot/scene.gltf", (gltf) => {
-        // gltf.scene.scale.set(0.1,0.1,0.1)
-        gltf.scene.position.set(3, 0, 0)
+        gltf.scene.position.set(2.5, -0.75, 0)
         scene.add(gltf.scene)
 
         mixer = new THREE.AnimationMixer(gltf.scene); // 假设您想对整个场景进行动画。
@@ -282,7 +317,11 @@ onMounted(() => {
         }
     });
 
-
+    //修改评论区背景颜色 和按钮背景颜色
+    const elInputWrappers = document.querySelectorAll('.el-input__wrapper');
+    elInputWrappers.forEach(wrapper => {
+        wrapper.style.backgroundColor = 'transparent';
+    });
 
 })
 
@@ -290,7 +329,7 @@ onMounted(() => {
 </script>
 <style  scoped>
 .wrap {
-    width: 1494px;
+    width: 1690px;
     height: 100vh;
     position: relative;
     perspective: 1000px;
@@ -303,7 +342,7 @@ onMounted(() => {
 }
 
 .intro-container {
-    width: 1494px;
+    width: 1690px;
     height: 100vh;
     overflow: hidden !important;
     margin: 0 auto;
@@ -316,7 +355,7 @@ onMounted(() => {
     font-size: 100px;
     background-image: linear-gradient(to top, pink, violet);
     -webkit-background-clip: text;
-    background-clip: text; 
+    background-clip: text;
     color: transparent;
     display: block;
 }
@@ -325,7 +364,7 @@ onMounted(() => {
     font-size: 30px;
     background-image: linear-gradient(to top, pink, violet);
     -webkit-background-clip: text;
-    background-clip: text; 
+    background-clip: text;
     color: transparent;
 }
 
@@ -338,6 +377,7 @@ onMounted(() => {
     height: 55vh;
     position: absolute;
     top: 20vh;
+    left: 60px;
 }
 
 .scrollCharacter-container li {
@@ -349,6 +389,7 @@ onMounted(() => {
     opacity: 0.3;
     overflow-wrap: break-word;
     text-align: justify;
+    list-style-type: none
 }
 
 .tour-container {
@@ -379,6 +420,7 @@ onMounted(() => {
     font-weight: 700;
     color: #fff;
     opacity: 0.8;
+    list-style-type: none;
     overflow-wrap: break-word;
 }
 
@@ -405,6 +447,8 @@ onMounted(() => {
     color: #fff;
     opacity: 0.8;
     overflow-wrap: break-word;
+    list-style-type: none;
+
 }
 
 .France-tour {
@@ -430,6 +474,8 @@ onMounted(() => {
     color: #fff;
     opacity: 0.8;
     overflow-wrap: break-word;
+    list-style-type: none;
+
 }
 
 .France-more-information {
@@ -493,29 +539,75 @@ onMounted(() => {
     left: 65px;
     opacity: 1;
 }
-.UK-name{
+
+.UK-name {
     font-size: 80px;
-    background-image: linear-gradient(to top,#f0932b, #ffbe76);
+    background-image: linear-gradient(to top, #f0932b, #ffbe76);
     -webkit-background-clip: text;
-    background-clip: text; 
+    background-clip: text;
     color: transparent;
     display: block;
     padding-left: 6px;
 }
-.Spain-name{
+
+.Spain-name {
     font-size: 80px;
-    background-image: linear-gradient(to top,#eb4d4b, #ff7979);
+    background-image: linear-gradient(to top, #eb4d4b, #ff7979);
     -webkit-background-clip: text;
-    background-clip: text; 
+    background-clip: text;
     color: transparent;
     display: block;
 }
-.France-name{
+
+.France-name {
     font-size: 80px;
-    background-image: linear-gradient(to top,#22a6b3, #7ed6df);
-    -webkit-background-clip: text; 
-    background-clip: text; 
+    background-image: linear-gradient(to top, #22a6b3, #7ed6df);
+    -webkit-background-clip: text;
+    background-clip: text;
     color: transparent;
     display: block;
+}
+
+.message-board {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 880px;
+    height: 380px;
+}
+
+.logo {
+    position: absolute;
+    display: inline;
+    background: url("/logo.png");
+    width: 80px;
+    height: 80px;
+    background-size: cover;
+}
+
+.logo-container {
+    width: 300px;
+    height: 80px;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+
+}
+
+.logo-container span {
+    position: absolute;
+    left: 80px;
+    top: 30px;
+    font-size: 20px;
+    background-image: linear-gradient(to right, #f39c12, #f1c40f);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+}
+
+.comment-input-conainer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
 }
 </style>
