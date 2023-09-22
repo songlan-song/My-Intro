48aa33a73756e71134102915e79694bcec3bc56a
lan song <lansongbjut@163.com>
2023-09-22 01:28:54 +0100

    finish chracter intro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,73 @@
 <template>
     <div class="home">
         <div class="wrap">
-            <div class="intro-container"  id="intro-container">
-            <div class="canvas-container" ref="screenDom" id="canvas-container" ></div>
-            <div class="scrollCharacter-container">
-                <ul class="all-charters">
-                    <li class="text1" ref="text1" style="opacity: 1;">1111111111111111111111111111111</li>
-                    <li class="text2" ref="text2">2222222222222222222222222222222</li>
-                    <li class="text3" ref="text3">3333333333333333333333333333333</li>
-                    <li class="text4" ref="text4">4444444444444444444444444444444</li>
-                </ul>
+            <div class="intro-container" id="intro-container">
+                <div class="canvas-container" ref="screenDom" id="canvas-container"></div>
+                <div class="scrollCharacter-container">
+                    <span style="font-size: 54px ;color: #fff;display: block;">Hi，你好! 我是</span>
+                    <span class="name">Lan Song</span>
+                    <span class="job">前端开发</span>
+                    <ul class="all-charters">
+                        <li class="text1" ref="text1" style="opacity: 1;">一个简单的代码爱好者,热衷于学习前端的各种技术，目前是一个前端开发工程师。</li>
+                        <li class="text2" ref="text2">2022年本科毕业于北京工业大学测控技术与仪器专业，2023年硕士毕业于伯明翰大学电子与计算机工程专业。</li>
+                        <li class="text3" ref="text3">乐观和热情是我的礼物，不敲代码的时候，有时是玩家，有时是运动员。热爱LOL，热爱篮球，偶尔看剧，喜欢美食。</li>
+                        <li class="text4" ref="text4">如果你也喜欢开源，喜欢创造，喜欢分享，欢迎在我的网站留言。</li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </div>
         <div class="tour-container">
             <div class="UK-tour">
                 <div class="UK-chracter">
+                    <span class="UK-name">伦敦</span>
                     <ul>
-                    <li>1111111111111111111111111111111</li>
-                    <li>2222222222222222222222222222222</li>
-                    <li>3333333333333333333333333333333</li>
-                    <li>4444444444444444444444444444444</li>
+                        <li>伦敦，这座历史悠久而又充满活力的城市，以其独特的魅力吸引着世界各地的游客。伦敦不仅是英国的首都，更是一个文化、艺术、历史和时尚的大熔炉，这里汇聚了无数的人文景观和自然美景，为每一位来访者提供了丰富多彩的体验。</li>
+                        <li>漫步在泰晤士河畔，您可以欣赏到大本钟和伦敦眼的壮丽景象，感受这座城市独特的历史韵味和现代气息的完美融合。白金汉宫的卫兵换岗仪式，是体验英国皇家传统的不二之选。塔桥更是以其独特的设计和卓越的工程技术成为了伦敦的象征之一。来到历史悠久的贝克街，仿佛能感受到福尔摩斯侦探故事中的神秘氛围。这里，每一扇门后都似乎藏着未解之谜，每一道巷口都充满了侦探故事的味道。</li>
                     </ul>
                 </div>
-                <div class="UK-more-information" ref="franceMoreInformation" ></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
-                <div class="moreinfo-character">
-                    <span>探索更多</span>
-                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
-                </div>
+                <div class="UK-more-information" ref="franceMoreInformation"></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                    <div class="moreinfo-character">
+                        <span>探索更多</span>
+                        <TopRight
+                            style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                    </div>
                 </div>
             </div>
             <div class="Spain-tour">
                 <div class="Spain-chracter">
+                    <span class="Spain-name">巴塞罗那</span>
                     <ul>
-                    <li>1111111111111111111111111111111</li>
-                    <li>2222222222222222222222222222222</li>
-                    <li>3333333333333333333333333333333</li>
-                    <li>4444444444444444444444444444444</li>
+                        <li>毫无疑问，巴塞罗那是一座充满艺术氛围、热情开放的城市，她位于西班牙东北部，坐拥美丽的地中海海岸线。这座城市以其丰富的历史、独特的建筑、美味的美食和活力四射的文化生活而闻名于世。</li>
+                        <li>巴塞罗那是世界著名建筑大师安东尼·高迪的艺术殿堂。当您漫步在这座城市的大街小巷时，会不时地遇见高迪独具一格的建筑作品。其中，圣家堂无疑是最为人们熟知的地标之一，其独特的设计和宏伟的塔尖令人叹为观止。除此之外，帕克·吉厄尔、米拉之家和巴特约之家等都是高迪留给这座城市的珍贵遗产。</li>
                     </ul>
                 </div>
-                <div class="Spain-more-information" ref="franceMoreInformation" ></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
-                <div class="moreinfo-character">
-                    <span>探索更多</span>
-                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
-                </div>
+                <div class="Spain-more-information" ref="franceMoreInformation"></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                    <div class="moreinfo-character">
+                        <span>探索更多</span>
+                        <TopRight
+                            style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                    </div>
                 </div>
             </div>
             <div class="France-tour">
                 <div class="France-chracter">
+                    <span class="France-name">巴黎</span>
                     <ul>
-                    <li>1111111111111111111111111111111</li>
-                    <li>2222222222222222222222222222222</li>
-                    <li>3333333333333333333333333333333</li>
-                    <li>4444444444444444444444444444444</li>
+                        <li>巴黎，被誉为“光之城”，是一座充满浪漫与艺术氛围的城市。这里是时尚的发源地，艺术的殿堂，美食的天堂。巴黎的每一条街道、每一座建筑都仿佛在讲述着一段古老而浪漫的故事。</li>
+                        <li>艾菲尔铁塔是巴黎乃至全球最为人们所熟知的地标之一，她伫立在塞纳河畔，以其优雅的姿态和独特的设计吸引着世界各地的游客。当夜幕降临，艾菲尔铁塔上的灯光闪烁，整个城市都仿佛被浪漫的氛围所包围。</li>
+                        <li>巴黎还是世界著名的艺术之都。卢浮宫博物馆收藏着丰富的艺术珍品，如《蒙娜丽莎》和《胜利女神像》等，是艺术爱好者的必游之地。</li>
+                        <li>这座城市无论在哪个角落，都充满了浪漫和诗意，无论是漫步在塞纳河畔，还是欣赏夜晚的巴黎，都会让您感受到这座城市独特的魅力，留下难以忘怀的回忆。</li>
                     </ul>
                 </div>
-                <div class="France-more-information" ref="franceMoreInformation" ></div>
-                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave ="handleMouseLeave" :style = {opacity:imageOpacity}>
-                <div class="moreinfo-character">
-                    <span>探索更多</span>
-                    <TopRight style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
-                </div>
+                <div class="France-more-information" ref="franceMoreInformation"></div>
+                <div class="cover-black" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" :style="{ opacity: imageOpacity }">
+                    <div class="moreinfo-character">
+                        <span>探索更多</span>
+                        <TopRight
+                            style="width: 1.3em; height: 1.3em; color: #fff;position: absolute;right: 55px;top: 1px;" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,12 +87,12 @@ const text2 = ref(null)
 const text3 = ref(null)
 const text4 = ref(null)
 const imageOpacity = ref(0)
-function handleMouseEnter(){
-        imageOpacity.value=0.4
-        
-    }
-function handleMouseLeave(){
-    imageOpacity.value=0
+
+function handleMouseEnter() {
+    imageOpacity.value = 0.4
+}
+function handleMouseLeave() {
+    imageOpacity.value = 0
 }
 onMounted(() => {
     //创建场景
@@ -207,7 +212,10 @@ onMounted(() => {
         }
         renderer.render(scene, camera)
     }
+
+    //当用户往下滚动看不到3d渲染界面式，不再渲染
     render()
+
 
     //创建光源
     let light1 = new THREE.DirectionalLight(0xffffff, 1)
@@ -233,46 +241,46 @@ onMounted(() => {
     //文字滚动高亮效果
     gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.create({
-    trigger: ".wrap",
-    start: "top top",
-    end: "+=800",
-    scrub: true,
-    pin: true,
-    animation: gsap.timeline()
-        .to(".text1", { opacity: 1, duration: 1 })
-        .to(".text1", { opacity: 0.3, duration: 1 })
-        .to(".text2", { opacity: 1, duration: 1.5 })
-        .to(".text2", { opacity: 0.3, duration: 1.5 })
-        .to(".text3", { opacity: 1, duration: 1.5 })
-        .to(".text3", { opacity: 0.3, duration: 1.5 })
-        .to(".text4", { opacity: 1, duration: 1 })
-});
+        trigger: ".wrap",
+        start: "top top",
+        end: "+=800",
+        scrub: true,
+        pin: true,
+        animation: gsap.timeline()
+            .to(".text1", { opacity: 1, duration: 1 })
+            .to(".text1", { opacity: 0.3, duration: 1 })
+            .to(".text2", { opacity: 1, duration: 1.5 })
+            .to(".text2", { opacity: 0.3, duration: 1.5 })
+            .to(".text3", { opacity: 1, duration: 1.5 })
+            .to(".text3", { opacity: 0.3, duration: 1.5 })
+            .to(".text4", { opacity: 1, duration: 1 })
+    });
 
     //intro-container淡出效果
     ScrollTrigger.create({
-    trigger: ".tour-container",
-    start: "top 800px",
-    end: "+=800",
-    scrub: true,
-    onEnter: () => {
-        gsap.to(".intro-container", {
-            bottom:"-130px",
-            scale: 0.8,
-            transformOrigin: "center center",
-            duration: 0.5,
-            transform: "translateZ(-200px)"
-        });
-    },
-    onLeaveBack: () => {
-        gsap.to(".intro-container", {
-            scale: 1,
-            bottom:0,
-            transformOrigin: "center center",
-            duration: 0.5,
-            transform: "translateZ(0px)"
-        });
-    }
-});
+        trigger: ".tour-container",
+        start: "top 800px",
+        end: "+=800",
+        scrub: true,
+        onEnter: () => {
+            gsap.to(".intro-container", {
+                bottom: "-130px",
+                scale: 0.8,
+                transformOrigin: "center center",
+                duration: 0.5,
+                transform: "translateZ(-200px)"
+            });
+        },
+        onLeaveBack: () => {
+            gsap.to(".intro-container", {
+                scale: 1,
+                bottom: 0,
+                transformOrigin: "center center",
+                duration: 0.5,
+                transform: "translateZ(0px)"
+            });
+        }
+    });
 
 
 
@@ -281,9 +289,7 @@ onMounted(() => {
 
 </script>
 <style  scoped>
-
-
-.wrap{
+.wrap {
     width: 1494px;
     height: 100vh;
     position: relative;
@@ -292,17 +298,35 @@ onMounted(() => {
 
 }
 
-.canvas-container{
+.canvas-container {
     overflow-x: hidden;
 }
-.intro-container{
+
+.intro-container {
     width: 1494px;
     height: 100vh;
     overflow: hidden !important;
-    margin:0 auto;
+    margin: 0 auto;
     transform-origin: center center;
     position: absolute;
     overflow-x: hidden;
+}
+
+.name {
+    font-size: 100px;
+    background-image: linear-gradient(to top, pink, violet);
+    -webkit-background-clip: text;
+    background-clip: text; 
+    color: transparent;
+    display: block;
+}
+
+.job {
+    font-size: 30px;
+    background-image: linear-gradient(to top, pink, violet);
+    -webkit-background-clip: text;
+    background-clip: text; 
+    color: transparent;
 }
 
 .home {
@@ -310,45 +334,48 @@ onMounted(() => {
 }
 
 .scrollCharacter-container {
-    width: 40vw;
+    width: 44vw;
     height: 55vh;
     position: absolute;
     top: 20vh;
 }
 
 .scrollCharacter-container li {
-    display: inline;
     line-height: 1.2;
     font-weight: 600;
-    font-size: 48px;
+    font-size: 30px;
     font-weight: 700;
     color: #fff;
     opacity: 0.3;
     overflow-wrap: break-word;
+    text-align: justify;
 }
-.tour-container{
+
+.tour-container {
     width: 100%;
     height: 100vh;
     margin-top: 0;
 }
+
 .UK-tour {
     width: 100%;
     height: 100vh;
-    background:url("/UK/2.png");
+    background: url("/UK/2.png");
     background-size: cover;
     position: relative;
 }
-.UK-chracter{
+
+.UK-chracter {
     width: 30vw;
     height: 55vh;
     position: absolute;
-    top: 20vh;
+    top: 5vh;
 }
-.UK-chracter li{
-    display: inline;
+
+.UK-chracter li {
     line-height: 1.2;
     font-weight: 600;
-    font-size: 48px;
+    font-size: 20px;
     font-weight: 700;
     color: #fff;
     opacity: 0.8;
@@ -358,21 +385,22 @@ onMounted(() => {
 .Spain-tour {
     width: 100%;
     height: 100vh;
-    background:url("/Spain/4.jpg");
+    background: url("/Spain/4.jpg");
     background-size: cover;
     position: relative;
 }
-.Spain-chracter{
+
+.Spain-chracter {
     width: 30vw;
     height: 55vh;
     position: absolute;
-    top: 20vh;
+    top: 5vh;
 }
-.Spain-chracter li{
-    display: inline;
+
+.Spain-chracter li {
     line-height: 1.2;
     font-weight: 600;
-    font-size: 48px;
+    font-size: 20px;
     font-weight: 700;
     color: #fff;
     opacity: 0.8;
@@ -380,29 +408,31 @@ onMounted(() => {
 }
 
 .France-tour {
-    width:100%;
+    width: 100%;
     height: 100vh;
-    background:url("/France/2.jpg");
+    background: url("/France/2.jpg");
     background-size: cover;
     position: relative;
 }
-.France-chracter{
+
+.France-chracter {
     width: 30vw;
     height: 55vh;
     position: absolute;
-    top: 20vh;
+    top: 5vh;
 }
-.France-chracter li{
-    display: inline;
+
+.France-chracter li {
     line-height: 1.2;
     font-weight: 600;
-    font-size: 48px;
+    font-size: 20px;
     font-weight: 700;
     color: #fff;
     opacity: 0.8;
     overflow-wrap: break-word;
 }
-.France-more-information{
+
+.France-more-information {
     position: absolute;
     right: 20px;
     top: 20px;
@@ -412,7 +442,8 @@ onMounted(() => {
     background-size: cover;
     cursor: pointer;
 }
-.UK-more-information{
+
+.UK-more-information {
     position: absolute;
     right: 20px;
     top: 20px;
@@ -422,7 +453,8 @@ onMounted(() => {
     background-size: cover;
     cursor: pointer;
 }
-.Spain-more-information{
+
+.Spain-more-information {
     position: absolute;
     right: 20px;
     top: 20px;
@@ -433,7 +465,7 @@ onMounted(() => {
     cursor: pointer;
 }
 
-.cover-black{
+.cover-black {
     position: absolute;
     right: 20px;
     top: 20px;
@@ -444,21 +476,45 @@ onMounted(() => {
     opacity: 0;
     cursor: pointer;
 }
-.moreinfo-character{
-    position:absolute;
+
+.moreinfo-character {
+    position: absolute;
     bottom: 0;
     width: 100%;
     height: 30px;
     margin-bottom: 10px;
 
 }
-.moreinfo-character span{
+
+.moreinfo-character span {
     color: #fff;
     font-weight: 700;
     position: absolute;
     left: 65px;
     opacity: 1;
 }
-
-
+.UK-name{
+    font-size: 80px;
+    background-image: linear-gradient(to top,#f0932b, #ffbe76);
+    -webkit-background-clip: text;
+    background-clip: text; 
+    color: transparent;
+    display: block;
+}
+.Spain-name{
+    font-size: 80px;
+    background-image: linear-gradient(to top,#eb4d4b, #ff7979);
+    -webkit-background-clip: text;
+    background-clip: text; 
+    color: transparent;
+    display: block;
+}
+.France-name{
+    font-size: 80px;
+    background-image: linear-gradient(to top,#22a6b3, #7ed6df);
+    -webkit-background-clip: text; 
+    background-clip: text; 
+    color: transparent;
+    display: block;
+}
 </style>
